feea957edfd36a05f69081fe9f59ebe20d39fdd0
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-02 21:54:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ text = ""
 # i = 0
 # while i < arr.length
 arr.each do |string| 
- text = [arr[0].reverse, arr[1].reverse, arr[2].reverse, arr[3].reverse, arr[4].reverse, arr[5].reverse, arr[6].reverse ]
+ text = [arr[0].reverse, arr[1].reverse, arr[2].reverse, arr[3].reverse, arr[4].reverse, arr[5].reverse, ]
  text
 #  i += 1
   end 
