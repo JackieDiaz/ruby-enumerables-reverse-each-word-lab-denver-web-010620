3a025bdf4cb9b0b133fccf035cb63424d8c2ce70
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-02 21:52:26 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,29 @@
+def reverse_each_word(string)
+arr = string.split 
+text = ""
+# i = 0
+# while i < arr.length
+arr.each do |string| 
+ text = [arr[0].reverse, arr[1].reverse, arr[2].reverse, arr[3].reverse, arr[4].reverse, arr[5].reverse]
+ text
+#  i += 1
+  end 
+text.join(' ')
+ end 
+
+
+
+  
+# require "pry"
+# def reverse_each_word(string)
+# arr = string.split 
+# text = ""
+# i = 0
+# while i < arr.length
+# arr.each do 
+# text = string.reverse
+# i += 1
+#   end 
+# end 
+# text
+# end
